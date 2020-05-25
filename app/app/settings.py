e4e598fd5223c8ef6b84d3ed653e3ac32ca81e1f
hunny9891 <hunny9891@gmail.com>
2020-05-25 16:23:16 +0530

    Bug Fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'snxgdrfda(i0@0_*+$goi0v9abeele_3tj!k)fxjy@+dmexf+r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
