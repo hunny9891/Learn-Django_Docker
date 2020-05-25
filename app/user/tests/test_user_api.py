cadbbd9741e74b53075141e99fe03f27eda3f1ac
hunny9891 <hunny9891@gmail.com>
2020-05-25 18:11:05 +0530

    bug fix

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.url import reverse
+from django.urls import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
